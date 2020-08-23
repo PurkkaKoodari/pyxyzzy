02e34ce8bcb9fbd1e7e62b70056bf5065cda5c25
PurkkaKoodari <purkka@purkka.codes>
2020-08-23 17:41:24 +0300

    Update login screen text, add git link

--- client/src/components/LoginScreen.tsx
+++ client/src/components/LoginScreen.tsx
@@ -25,7 +25,7 @@ const LoginScreen = () => {
     nameProblems.push("Your name can't end with a space.")
   if (name.includes("  "))
     nameProblems.push("Your name can't contain two spaces in a row.")
-  
+
   const canSubmit = !loggingIn && nameProblems.length === 0
 
   if (loginError)
@@ -45,43 +45,46 @@ const LoginScreen = () => {
     }
     setLoggingIn(false)
   }
-  
+
   let nameProblemList = null
   if (name !== "" && nameProblems.length) {
     nameProblemList = (
-      <ul className="errors">
-        {nameProblems.map(problem => (<li key={problem}>{problem}</li>))}
-      </ul>
+        <ul className="errors">
+          {nameProblems.map(problem => (<li key={problem}>{problem}</li>))}
+        </ul>
     )
   }
 
   return (
-    <div className="login">
-      <h1>pyXyzzy</h1>
-      <p className="help-text">
-        pyXyzzy is a <ExternalLink href="https://www.cardsagainsthumanity.com/">Cards Against Humanity</ExternalLink> clone,
-        modeled after <ExternalLink href="https://github.com/ajanata/PretendYoureXyzzy">Pretend You're Xyzzy</ExternalLink>.
-      </p>
-      <p className="help-text">Choose a name to start playing.</p>
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          id="login-name"
-          placeholder="Name"
-          disabled={loggingIn}
-          maxLength={config.users.username.length.max}
-          value={name}
-          onChange={(e) => setName(e.target.value)} />
-        <button type="submit" disabled={!canSubmit}>Play</button>
-      </form>
-      {nameProblemList}
-      <p className="legal">
-        pyXyzzy is a clone of <ExternalLink href="https://www.cardsagainsthumanity.com/">Cards Against Humanity</ExternalLink>, which
-        is available under the <ExternalLink href="https://creativecommons.org/licenses/by-nc-sa/2.0/">CC BY-NC-SA 2.0</ExternalLink> license.
-        The source code is available on <ExternalLink href="#">TODO</ExternalLink> and licensed under
-        the <ExternalLink href="https://opensource.org/licenses/MIT">MIT license</ExternalLink>.
-      </p>
-    </div>
+      <div className="login">
+        <h1>pyXyzzy</h1>
+        <p className="help-text">
+          pyXyzzy is a <ExternalLink href="https://www.cardsagainsthumanity.com/">Cards Against Humanity
+          </ExternalLink> clone, modeled after <ExternalLink href="https://github.com/ajanata/PretendYoureXyzzy">
+          Pretend You're Xyzzy</ExternalLink> but completely rewritten for a modern experience.
+        </p>
+        <p className="help-text">Choose a name to start playing.</p>
+        <form onSubmit={handleLogin}>
+          <input
+              type="text"
+              id="login-name"
+              placeholder="Name"
+              disabled={loggingIn}
+              maxLength={config.users.username.length.max}
+              value={name}
+              onChange={(e) => setName(e.target.value)} />
+          <button type="submit" disabled={!canSubmit}>Play</button>
+        </form>
+        {nameProblemList}
+        <p className="legal">
+          pyXyzzy is a clone of <ExternalLink href="https://www.cardsagainsthumanity.com/">Cards Against Humanity
+          </ExternalLink> and uses its cards and rules, available under
+          the <ExternalLink href="https://creativecommons.org/licenses/by-nc-sa/2.0/">CC BY-NC-SA 2.0
+          </ExternalLink> license. The source code is available
+          on <ExternalLink href="https://gitlab.com/PurkkaKoodari/pyxyzzy">GitLab</ExternalLink> and licensed under
+          the <ExternalLink href="https://opensource.org/licenses/MIT">MIT license</ExternalLink>.
+        </p>
+      </div>
   )
 }
 
